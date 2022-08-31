1017e888d59d6d57eb092c28fce70ee6f251b7f5
John Paxton <johnpaxton@gmail.com>
2022-08-31 10:20:20 -0400

    Cleaned out code to its own file

--- rxjava-demos/src/main/java/demos/FirstObservable.java
+++ rxjava-demos/src/main/java/demos/FirstObservable.java
@@ -1,62 +1,10 @@
 package demos;
 
-import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Observable;
-import io.reactivex.rxjava3.core.Observer;
-import io.reactivex.rxjava3.disposables.Disposable;
-import org.reactivestreams.Subscriber;
-import org.reactivestreams.Subscription;
 
 public class FirstObservable {
-
-  public static void counter() {
-    // This would be what a Publisher wraps around
-    int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
-    for (int x: numbers) {
-      System.out.println("Received values: " + x);
-    }
-  }
-
-  public static void counterAsPublisher(Subscriber<Integer> sub) {
-    sub.onSubscribe((Subscription) sub);
-
-    int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
-    for (int x: numbers) {
-      sub.onNext(x);
-    }
-
-    sub.onComplete();
-
-  }
-
-  public static void subscribeToCounter() {
-    class CustomSubscriber implements Subscriber<Integer> {
-
-      @Override
-      public void onSubscribe(Subscription subscription) {
-
-      }
-
-      @Override
-      public void onNext(Integer integer) {
-        System.out.println("Next value: " + integer);
-      }
-
-      @Override
-      public void onError(Throwable throwable) {
-        System.err.println("Something went wrong!");
-      }
-
-      @Override
-      public void onComplete() {
-        System.out.println("Finished!");
-      }
-    }
-
-    counterAsPublisher(new CustomSubscriber());
-  }
+  // If you want to see a basic version of Reactive Streams' interfaces
+  // Look at ReactiveStreamsBasics in this package
 
   public static void main(String[] args) {
 
@@ -67,42 +15,18 @@ public class FirstObservable {
     // Three methods: onNext, onError, onComplete
     // onError and onComplete are terminators (they run when the Publisher
     // finishes unsuccessfully or successfully)
+    // Check out the overrides in the docs as well
     numbers.subscribe(i -> {
-      System.out.println("Received value: " + i);
-    }, error -> {
-      System.err.println("Something went wrong!");
-      System.err.println(error);
-    }, () -> {
-      System.out.println("Publisher has finished");
-    });
+                        System.out.println("Received value: " + i);
+                      },
+                      error -> {
+                        System.err.println("Something went wrong!");
+                        System.err.println(error.getLocalizedMessage());
+                      },
+                      () -> {
+                        System.out.println("Publisher has finished");
+                      });
 
-    // We can write a Subscriber class if we'd like
-    // CustomObserver co = new CustomObserver();
-    // numbers.subscribe(co);
   }
 
-  // Observers are Subscribers
-  static class CustomObserver implements Observer<Integer> {
-
-    @Override
-    public void onSubscribe(@NonNull Disposable d) {
-      System.out.println("Successfully subscribed");
-    }
-
-    @Override
-    public void onNext(@NonNull Integer integer) {
-      System.out.println("CustomObserver.onNext(): " + integer);
-    }
-
-    @Override
-    public void onError(@NonNull Throwable e) {
-      System.err.println("CustomObserver.onError(): " + e);
-
-    }
-
-    @Override
-    public void onComplete() {
-      System.out.println("CustomObserver.onComplete(): finished");
-    }
-  }
 }
